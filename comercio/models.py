97be3ced7f334ce6d8989d935eefabc8d24a5172
Edison Coronel <edisoncor@gmail.com>
2019-11-13 11:23:08 -0500

    Ejemplo de tios

--- comercio/models.py
+++ comercio/models.py
@@ -7,3 +7,43 @@ class Local(models.Model):
     """
     nombre = models.CharField(max_length=250)
 
+
+class Abuelo(models.Model):
+    nombre = models.CharField(max_length=250)
+
+
+class Padre(models.Model):
+    nombre = models.CharField(max_length=250)
+    abuelo = models.ForeignKey('Abuelo',
+                               related_name='padres',
+                               on_delete=models.CASCADE
+                               )
+
+
+class Tio(models.Model):
+    nombre = models.CharField(max_length=250)
+    abuelo = models.ForeignKey('Abuelo',
+                               related_name='tios',
+                               on_delete=models.CASCADE
+                               )
+
+
+class Primo(models.Model):
+    nombre = models.CharField(max_length=250)
+    tio = models.ForeignKey('Tio',
+                            related_name='primos',
+                            on_delete=models.CASCADE
+                            )
+
+
+class Hijo(models.Model):
+    nombre = models.CharField(max_length=250)
+    padre = models.ForeignKey('Padre',
+                              related_name='hijos',
+                              on_delete=models.CASCADE
+                              )
+    def abuelo(self):
+        return self.padre.abuelo
+
+    def tios(self):
+        return self.abuelo().tios.all()
